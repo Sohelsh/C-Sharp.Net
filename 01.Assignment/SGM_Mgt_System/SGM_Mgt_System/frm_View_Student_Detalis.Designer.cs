1b09cfe1bd428756d181510a981e6784340386a4
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-04 16:39:06 +0530

    Assignment_1

---
+++ 01.Assignment/SGM_Mgt_System/SGM_Mgt_System/frm_View_Student_Detalis.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SGM_Mgt_System
+{
+    partial class frm_View_Student_Detalis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lbl_Head = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_Add_Student_Details = new System.Windows.Forms.Button();
+            this.btn_Logout = new System.Windows.Forms.Button();
+            this.sGM_Students_Detelis_dbDataSet = new SGM_Mgt_System.SGM_Students_Detelis_dbDataSet();
+            this.sGMStudentDetalisBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sGM_Student_DetalisTableAdapter = new SGM_Mgt_System.SGM_Students_Detelis_dbDataSetTableAdapters.SGM_Student_DetalisTableAdapter();
+            this.rollNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dOBDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mobileNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.courseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sGM_Students_Detelis_dbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sGMStudentDetalisBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Head
+            // 
+            this.lbl_Head.AutoSize = true;
+            this.lbl_Head.BackColor = System.Drawing.Color.Pink;
+            this.lbl_Head.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Head.Location = new System.Drawing.Point(297, 35);
+            this.lbl_Head.Name = "lbl_Head";
+            this.lbl_Head.Size = new System.Drawing.Size(647, 76);
+            this.lbl_Head.TabIndex = 5;
+            this.lbl_Head.Text = "View Student Details";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.rollNoDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.dOBDataGridViewTextBoxColumn,
+            this.mobileNoDataGridViewTextBoxColumn,
+            this.courseDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.sGMStudentDetalisBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(50, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1129, 507);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // btn_Add_Student_Details
+            // 
+            this.btn_Add_Student_Details.BackColor = System.Drawing.Color.Pink;
+            this.btn_Add_Student_Details.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add_Student_Details.Location = new System.Drawing.Point(352, 662);
+            this.btn_Add_Student_Details.Name = "btn_Add_Student_Details";
+            this.btn_Add_Student_Details.Size = new System.Drawing.Size(543, 69);
+            this.btn_Add_Student_Details.TabIndex = 1;
+            this.btn_Add_Student_Details.Text = "Add Student Details";
+            this.btn_Add_Student_Details.UseVisualStyleBackColor = false;
+            this.btn_Add_Student_Details.Click += new System.EventHandler(this.btn_Add_Student_Details_Click);
+            // 
+            // btn_Logout
+            // 
+            this.btn_Logout.BackColor = System.Drawing.Color.Crimson;
+            this.btn_Logout.Font = new System.Drawing.Font("Verdana", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Logout.Location = new System.Drawing.Point(1038, -4);
+            this.btn_Logout.Name = "btn_Logout";
+            this.btn_Logout.Size = new System.Drawing.Size(226, 61);
+            this.btn_Logout.TabIndex = 2;
+            this.btn_Logout.Text = "Logout";
+            this.btn_Logout.UseVisualStyleBackColor = false;
+            this.btn_Logout.Click += new System.EventHandler(this.btn_Logout_Click);
+            // 
+            // sGM_Students_Detelis_dbDataSet
+            // 
+            this.sGM_Students_Detelis_dbDataSet.DataSetName = "SGM_Students_Detelis_dbDataSet";
+            this.sGM_Students_Detelis_dbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sGMStudentDetalisBindingSource
+            // 
+            this.sGMStudentDetalisBindingSource.DataMember = "SGM_Student_Detalis";
+            this.sGMStudentDetalisBindingSource.DataSource = this.sGM_Students_Detelis_dbDataSet;
+            // 
+            // sGM_Student_DetalisTableAdapter
+            // 
+            this.sGM_Student_DetalisTableAdapter.ClearBeforeFill = true;
+            // 
+            // rollNoDataGridViewTextBoxColumn
+            // 
+            this.rollNoDataGridViewTextBoxColumn.DataPropertyName = "Roll_No";
+            this.rollNoDataGridViewTextBoxColumn.HeaderText = "Roll_No";
+            this.rollNoDataGridViewTextBoxColumn.Name = "rollNoDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // dOBDataGridViewTextBoxColumn
+            // 
+            this.dOBDataGridViewTextBoxColumn.DataPropertyName = "DOB";
+            this.dOBDataGridViewTextBoxColumn.HeaderText = "DOB";
+            this.dOBDataGridViewTextBoxColumn.Name = "dOBDataGridViewTextBoxColumn";
+            // 
+            // mobileNoDataGridViewTextBoxColumn
+            // 
+            this.mobileNoDataGridViewTextBoxColumn.DataPropertyName = "Mobile_No";
+            this.mobileNoDataGridViewTextBoxColumn.HeaderText = "Mobile_No";
+            this.mobileNoDataGridViewTextBoxColumn.Name = "mobileNoDataGridViewTextBoxColumn";
+            // 
+            // courseDataGridViewTextBoxColumn
+            // 
+            this.courseDataGridViewTextBoxColumn.DataPropertyName = "Course";
+            this.courseDataGridViewTextBoxColumn.HeaderText = "Course";
+            this.courseDataGridViewTextBoxColumn.Name = "courseDataGridViewTextBoxColumn";
+            // 
+            // frm_View_Student_Detalis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(1262, 743);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_Logout);
+            this.Controls.Add(this.btn_Add_Student_Details);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lbl_Head);
+            this.Name = "frm_View_Student_Detalis";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "View Student Detalis";
+            this.Load += new System.EventHandler(this.frm_View_Student_Detalis_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sGM_Students_Detelis_dbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sGMStudentDetalisBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Head;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_Add_Student_Details;
+        private System.Windows.Forms.Button btn_Logout;
+        private SGM_Students_Detelis_dbDataSet sGM_Students_Detelis_dbDataSet;
+        private System.Windows.Forms.BindingSource sGMStudentDetalisBindingSource;
+        private SGM_Students_Detelis_dbDataSetTableAdapters.SGM_Student_DetalisTableAdapter sGM_Student_DetalisTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rollNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dOBDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mobileNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseDataGridViewTextBoxColumn;
+    }
+}
---
+++ 01.Assignment/SGM_Mgt_System/SGM_Mgt_System/frm_View_Student_Detalis.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SGM_Mgt_System
+{
+    public partial class frm_View_Student_Detalis : Form
+    {
+        public frm_View_Student_Detalis()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Logout_Click(object sender, EventArgs e)
+        {
+            frm_Login_Page Obj = new frm_Login_Page();
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void btn_Add_Student_Details_Click(object sender, EventArgs e)
+        {
+            frm_Add_Student_Detalis Obj = new frm_Add_Student_Detalis();
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void frm_View_Student_Detalis_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'sGM_Students_Detelis_dbDataSet.SGM_Student_Detalis' table. You can move, or remove it, as needed.
+            this.sGM_Student_DetalisTableAdapter.Fill(this.sGM_Students_Detelis_dbDataSet.SGM_Student_Detalis);
+
+        }
+    }
+}
