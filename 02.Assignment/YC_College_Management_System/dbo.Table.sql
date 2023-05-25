524fc053c6d1aea074760b6e35a766d568ee201d
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-25 20:48:53 +0530

    D

--- 02.Assignment/YC_College_Management_System/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Roll_No] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(40) NOT NULL, 
-    [DOB] DATE NOT NULL, 
-    [Mob_No] NUMERIC(10) NOT NULL, 
-    [Course] NCHAR(40) NOT NULL
-)
