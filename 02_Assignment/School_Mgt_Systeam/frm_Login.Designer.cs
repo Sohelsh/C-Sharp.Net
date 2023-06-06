c4456ce28856b66c6fd41366657aa35042954175
Sohelsh <sohelshaikhadam@gmail.com>
2023-06-06 23:42:51 +0530

    Assignment 2

---
+++ 02_Assignment/School_Mgt_Systeam/frm_Login.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace School_Mgt_Systeam
+{
+    partial class frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Head = new System.Windows.Forms.Label();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.lbl_Error = new System.Windows.Forms.Label();
+            this.tb_Username = new System.Windows.Forms.TextBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Head
+            // 
+            this.lbl_Head.AutoSize = true;
+            this.lbl_Head.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_Head.Font = new System.Drawing.Font("Georgia", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Head.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_Head.Location = new System.Drawing.Point(469, 50);
+            this.lbl_Head.Name = "lbl_Head";
+            this.lbl_Head.Size = new System.Drawing.Size(161, 49);
+            this.lbl_Head.TabIndex = 0;
+            this.lbl_Head.Text = "LOGIN";
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_Username.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Username.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_Username.Location = new System.Drawing.Point(179, 222);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(143, 32);
+            this.lbl_Username.TabIndex = 0;
+            this.lbl_Username.Text = "Username";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_Password.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_Password.Location = new System.Drawing.Point(179, 373);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(133, 32);
+            this.lbl_Password.TabIndex = 0;
+            this.lbl_Password.Text = "Password";
+            // 
+            // lbl_Error
+            // 
+            this.lbl_Error.AutoSize = true;
+            this.lbl_Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Error.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.lbl_Error.Location = new System.Drawing.Point(419, 534);
+            this.lbl_Error.Name = "lbl_Error";
+            this.lbl_Error.Size = new System.Drawing.Size(268, 20);
+            this.lbl_Error.TabIndex = 0;
+            this.lbl_Error.Text = "Enter Valid Username && Password";
+            // 
+            // tb_Username
+            // 
+            this.tb_Username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Username.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Username.Location = new System.Drawing.Point(738, 220);
+            this.tb_Username.Name = "tb_Username";
+            this.tb_Username.Size = new System.Drawing.Size(278, 34);
+            this.tb_Username.TabIndex = 1;
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Password.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Password.Location = new System.Drawing.Point(738, 371);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.Size = new System.Drawing.Size(278, 34);
+            this.tb_Password.TabIndex = 2;
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btn_Submit.Font = new System.Drawing.Font("Georgia", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Submit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Submit.Location = new System.Drawing.Point(451, 601);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(202, 50);
+            this.btn_Submit.TabIndex = 3;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = false;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1124, 746);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_Username);
+            this.Controls.Add(this.lbl_Error);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.lbl_Head);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOGIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Head;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Label lbl_Error;
+        private System.Windows.Forms.TextBox tb_Username;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.Button btn_Submit;
+    }
+}
+
