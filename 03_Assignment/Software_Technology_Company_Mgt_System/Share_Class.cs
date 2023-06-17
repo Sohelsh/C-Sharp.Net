6b2aca7d13d12ae183ab1c2b3a54a896fe956dda
Sohelsh <sohelshaikhadam@gmail.com>
2023-06-17 21:10:06 +0530

    Assignment No 3

---
+++ 03_Assignment/Software_Technology_Company_Mgt_System/Share_Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_Technology_Company_Mgt_System
+{
+    class Share_Class
+    {
+        public static String Username = "";
+    }
+}
