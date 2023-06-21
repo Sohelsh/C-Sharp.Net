08a26950905b0a458a1391b1a994b8ac483ce9fe
Sohelsh <sohelshaikhadam@gmail.com>
2023-06-21 23:19:41 +0530

    Assignment No 3

---
+++ 03_Assignment/Software_Technology_Company_Mgt_System/frm_Login.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Software_Technology_Company_Mgt_System
+{
+    partial class frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Login));
+            this.pnl_Login = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.tb_Username = new System.Windows.Forms.TextBox();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.lbl_Head = new System.Windows.Forms.Label();
+            this.lbl_Reset = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_Error1 = new System.Windows.Forms.Label();
+            this.lbl_Error2 = new System.Windows.Forms.Label();
+            this.pnl_Login.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_Login
+            // 
+            this.pnl_Login.BackColor = System.Drawing.Color.SlateBlue;
+            this.pnl_Login.Controls.Add(this.pictureBox2);
+            this.pnl_Login.Location = new System.Drawing.Point(0, -3);
+            this.pnl_Login.Name = "pnl_Login";
+            this.pnl_Login.Size = new System.Drawing.Size(285, 585);
+            this.pnl_Login.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(81, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(101, 362);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 27;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Password.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Password.Location = new System.Drawing.Point(383, 377);
+            this.tb_Password.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.PasswordChar = '*';
+            this.tb_Password.Size = new System.Drawing.Size(333, 31);
+            this.tb_Password.TabIndex = 2;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.Location = new System.Drawing.Point(378, 333);
+            this.lbl_Password.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(101, 25);
+            this.lbl_Password.TabIndex = 22;
+            this.lbl_Password.Text = "Password";
+            // 
+            // tb_Username
+            // 
+            this.tb_Username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Username.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Username.Location = new System.Drawing.Point(383, 250);
+            this.tb_Username.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Username.Name = "tb_Username";
+            this.tb_Username.Size = new System.Drawing.Size(333, 31);
+            this.tb_Username.TabIndex = 1;
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Username.Location = new System.Drawing.Point(378, 208);
+            this.lbl_Username.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(108, 25);
+            this.lbl_Username.TabIndex = 21;
+            this.lbl_Username.Text = "Username";
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.SlateBlue;
+            this.btn_Login.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Login.Location = new System.Drawing.Point(483, 458);
+            this.btn_Login.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(120, 46);
+            this.btn_Login.TabIndex = 3;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // lbl_Head
+            // 
+            this.lbl_Head.AutoSize = true;
+            this.lbl_Head.Font = new System.Drawing.Font("Georgia", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Head.ForeColor = System.Drawing.Color.SlateBlue;
+            this.lbl_Head.Location = new System.Drawing.Point(328, 25);
+            this.lbl_Head.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Head.Name = "lbl_Head";
+            this.lbl_Head.Size = new System.Drawing.Size(441, 34);
+            this.lbl_Head.TabIndex = 24;
+            this.lbl_Head.Text = "Software Technologies PVT LTD";
+            // 
+            // lbl_Reset
+            // 
+            this.lbl_Reset.AutoSize = true;
+            this.lbl_Reset.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Reset.ForeColor = System.Drawing.Color.SlateBlue;
+            this.lbl_Reset.Location = new System.Drawing.Point(512, 517);
+            this.lbl_Reset.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Reset.Name = "lbl_Reset";
+            this.lbl_Reset.Size = new System.Drawing.Size(63, 25);
+            this.lbl_Reset.TabIndex = 4;
+            this.lbl_Reset.Text = "Reset";
+            this.lbl_Reset.Click += new System.EventHandler(this.lbl_Reset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(479, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 26;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_Error1
+            // 
+            this.lbl_Error1.AutoSize = true;
+            this.lbl_Error1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Error1.ForeColor = System.Drawing.Color.Gold;
+            this.lbl_Error1.Location = new System.Drawing.Point(565, 283);
+            this.lbl_Error1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Error1.Name = "lbl_Error1";
+            this.lbl_Error1.Size = new System.Drawing.Size(146, 16);
+            this.lbl_Error1.TabIndex = 4;
+            this.lbl_Error1.Text = "Enter Valid Username";
+            this.lbl_Error1.Visible = false;
+            // 
+            // lbl_Error2
+            // 
+            this.lbl_Error2.AutoSize = true;
+            this.lbl_Error2.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Error2.ForeColor = System.Drawing.Color.Gold;
+            this.lbl_Error2.Location = new System.Drawing.Point(565, 410);
+            this.lbl_Error2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Error2.Name = "lbl_Error2";
+            this.lbl_Error2.Size = new System.Drawing.Size(144, 16);
+            this.lbl_Error2.TabIndex = 4;
+            this.lbl_Error2.Text = "Enter Valid Password";
+            this.lbl_Error2.Visible = false;
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(830, 572);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_Error2);
+            this.Controls.Add(this.lbl_Error1);
+            this.Controls.Add(this.lbl_Reset);
+            this.Controls.Add(this.lbl_Head);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.tb_Username);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.pnl_Login);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_Login";
+            this.pnl_Login.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Login;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.TextBox tb_Username;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label lbl_Head;
+        private System.Windows.Forms.Label lbl_Reset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lbl_Error1;
+        private System.Windows.Forms.Label lbl_Error2;
+    }
+}
