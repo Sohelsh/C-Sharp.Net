c6a4f79d897470fb4568c459423d98880baf9a13
Sohelsh <sohelshaikhadam@gmail.com>
2023-08-17 22:13:16 +0530

    Assignment No 8

---
+++ 08_Assignment/User_Manage/frm_Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace User_Manage
+{
+    public partial class frm_Login : Form
+    {
+        public frm_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void cmb_Role_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Shared_Class.Bind_ComboBox(cmb_Username, "UserName", "SELECT DISTINCT (UserName) FROM Login_Details WHERE UserRole = '"+ cmb_Role.Text + "'");
+        }
+    }
+}
