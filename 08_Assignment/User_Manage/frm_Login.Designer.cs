44ff0503b00006c6d102b573408bc849b22c7c1b
Sohelsh <sohelshaikhadam@gmail.com>
2023-08-17 22:13:50 +0530

    Assignment No 8

---
+++ 08_Assignment/User_Manage/frm_Login.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace User_Manage
+{
+    partial class frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Head = new System.Windows.Forms.Label();
+            this.lbl_Role = new System.Windows.Forms.Label();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.cmb_Role = new System.Windows.Forms.ComboBox();
+            this.cmb_Username = new System.Windows.Forms.ComboBox();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Head
+            // 
+            this.lbl_Head.AutoSize = true;
+            this.lbl_Head.Font = new System.Drawing.Font("MS Reference Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Head.Location = new System.Drawing.Point(391, 42);
+            this.lbl_Head.Name = "lbl_Head";
+            this.lbl_Head.Size = new System.Drawing.Size(144, 46);
+            this.lbl_Head.TabIndex = 0;
+            this.lbl_Head.Text = "LOGIN";
+            // 
+            // lbl_Role
+            // 
+            this.lbl_Role.AutoSize = true;
+            this.lbl_Role.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Role.Location = new System.Drawing.Point(190, 168);
+            this.lbl_Role.Name = "lbl_Role";
+            this.lbl_Role.Size = new System.Drawing.Size(64, 29);
+            this.lbl_Role.TabIndex = 1;
+            this.lbl_Role.Text = "Role";
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Username.Location = new System.Drawing.Point(190, 239);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(124, 29);
+            this.lbl_Username.TabIndex = 1;
+            this.lbl_Username.Text = "Username";
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Submit.Location = new System.Drawing.Point(399, 476);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(136, 48);
+            this.btn_Submit.TabIndex = 3;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            // 
+            // cmb_Role
+            // 
+            this.cmb_Role.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_Role.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Role.FormattingEnabled = true;
+            this.cmb_Role.Items.AddRange(new object[] {
+            "Manager",
+            "Admin",
+            "Employee"});
+            this.cmb_Role.Location = new System.Drawing.Point(573, 173);
+            this.cmb_Role.Name = "cmb_Role";
+            this.cmb_Role.Size = new System.Drawing.Size(218, 37);
+            this.cmb_Role.TabIndex = 4;
+            this.cmb_Role.SelectedIndexChanged += new System.EventHandler(this.cmb_Role_SelectedIndexChanged);
+            // 
+            // cmb_Username
+            // 
+            this.cmb_Username.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Username.FormattingEnabled = true;
+            this.cmb_Username.Location = new System.Drawing.Point(573, 246);
+            this.cmb_Username.Name = "cmb_Username";
+            this.cmb_Username.Size = new System.Drawing.Size(218, 37);
+            this.cmb_Username.TabIndex = 4;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.Location = new System.Drawing.Point(190, 318);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(120, 29);
+            this.lbl_Password.TabIndex = 1;
+            this.lbl_Password.Text = "Password";
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Password.Location = new System.Drawing.Point(573, 325);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.Size = new System.Drawing.Size(218, 34);
+            this.tb_Password.TabIndex = 5;
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(1011, 609);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.cmb_Username);
+            this.Controls.Add(this.cmb_Role);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.lbl_Role);
+            this.Controls.Add(this.lbl_Head);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOGIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Head;
+        private System.Windows.Forms.Label lbl_Role;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.ComboBox cmb_Role;
+        private System.Windows.Forms.ComboBox cmb_Username;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.TextBox tb_Password;
+    }
+}
+
